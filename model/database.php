5d452a749fbed95e4dc2ba79b17f8424411634b3
moussaillon <100614972+Ramatoulaye23@users.noreply.github.com>
2024-06-19 17:24:22 +0200

    add password

--- model/database.php
+++ model/database.php
@@ -9,6 +9,7 @@ function dbConnect(){
     $dsn = 'mysql:dbname=fr_assos;host=127.0.0.1;port=3306';
     $user = 'root';
     $password = ''; // root sur macos avec mamp
+    $password = 'root'; // root sur macos avec mamp
 
     $database = new PDO($dsn, $user, $password);
     return $database;
